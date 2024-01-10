a6a44246b3c6498809066bfcf9c1cf919748e4a1
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-01-10 23:46:53 +0600

    adoptAnimalsCollection post route is created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -9,9 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cai2g.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +25,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const petsCollection = client.db("pet-adoption").collection("animals");
+    const adoptAnimalsCollection = client
+      .db("pet-adoption")
+      .collection("adoptAnimals");
+
+    app.get("/animals", async (req, res) => {
+      const result = await petsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await petsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/adoptAnimal", async (req, res) => {
+      const adoptAnimalData = req.body;
+      const result = await adoptAnimalsCollection.insertOne(adoptAnimalData);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,6 +59,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Pet is Running");
+});
 
 app.listen(port, () => {
   console.log(`Pet is Running ${port}`);
